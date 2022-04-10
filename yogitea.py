b80ed0acb298d27162bdca10c88c280a4afcf96d
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-04-10 20:04:48 +0200

    use console flow

--- yogitea.py
+++ yogitea.py
@@ -37,7 +37,9 @@ def get_yogitea_text():
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            # creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
+
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
